c37cfe3f0bb2740b02c8fcf22ce4defad9bd95bb
Abdelhadi Omar <abdelhadioumar@gmail.com>
2023-05-11 20:50:14 +0100

    random background generator

--- js/master.js
+++ js/master.js
@@ -1,5 +1,6 @@
-// check if there's a colo prop in local storage
+// check if there's a color prop in local storage
 let mainColor = localStorage.getItem('color-option');
+
 if (mainColor) {
     // if there is, add the color to the body
     document.documentElement.style.setProperty('--main-color', mainColor);
@@ -13,6 +14,37 @@ if (mainColor) {
     document.querySelectorAll('.colors-list li').forEach(color => {
         if (color.dataset.color === mainColor) {
             color.classList.add('active');
+        }
+    })
+}
+
+// random background option
+let randomBackground = true;
+
+// create a varibale that controls the interval
+let theInterval;
+
+// check if there's random background stored on local storage
+let backgroundLocalItem = localStorage.getItem('background_option');
+if (backgroundLocalItem) {
+    if (backgroundLocalItem === 'true') {
+        randomBackground = true;
+    } else {
+        randomBackground = false;
+    }
+
+    // remove active class
+    let rndom = document.querySelectorAll('.random-backgrounds span');
+    rndom.forEach(item => {
+        item.classList.remove('active');
+    });
+
+    // add active class
+    rndom.forEach(item => {
+        if (backgroundLocalItem === 'true') {
+            document.querySelector('.random-backgrounds .yes').classList.add('active');
+        } else {
+            document.querySelector('.random-backgrounds .no').classList.add('active');
         }
     })
 }
@@ -45,8 +77,32 @@ colorsList.forEach(color => {
 
         // add active class to the clicked item
         e.target.classList.add('active');
-    })
-})
+    });
+});
+
+//switch random background option
+const backgroundOption = document.querySelectorAll('.random-backgrounds span');
+backgroundOption.forEach(background => {
+    background.addEventListener('click', e => {
+        // remove active class
+        backgroundOption.forEach(back => {
+            back.classList.remove('active');
+        });
+
+        // add active class to the clicked item
+        e.target.classList.add('active');
+
+        if (e.target.dataset.background === 'yes') {
+            randomBackground = true;
+            randomizeImages();
+            localStorage.setItem('background_option', true);
+        } else {
+            randomBackground = false;
+            localStorage.setItem('background_option', false)
+            clearInterval(theInterval);
+        }
+    });
+});
 
 // select landing page element
 let landingPage = document.querySelector('.landing-page');
@@ -54,9 +110,14 @@ let landingPage = document.querySelector('.landing-page');
 // get array of images
 let imagesArray = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
 
-// change background image url
-setInterval(() => {
-    // get random number
-    let randomNumber = Math.floor(Math.random() * imagesArray.length);
-    landingPage.style.backgroundImage = `url('imgs/${imagesArray[randomNumber]}')`;
-}, 10000);
+// function to randomize images
+function randomizeImages() {
+    if (randomBackground) {
+        // change background image url
+        theInterval = setInterval(() => {
+            // get random number
+            let randomNumber = Math.floor(Math.random() * imagesArray.length);
+            landingPage.style.backgroundImage = `url('imgs/${imagesArray[randomNumber]}')`;
+        }, 1000);
+    }
+}
